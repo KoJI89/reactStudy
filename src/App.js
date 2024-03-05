aafc004c5e326c5fa5d73495c04aed067e30a610
fixedMan <fixed_man@kakao.com>
2024-03-05 12:28:56 +0900

    초기파일

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const numbers = [1,2,3,4,5];
+
+  // forEach
+  let total = 0;
+  numbers.forEach( numbers =>{
+    total += numbers;
+  });
+
+  // reduce
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {total}
     </div>
   );
 }
